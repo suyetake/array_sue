8f77dea1517d42edf9e2a8bdda35732f178c8bd8
Sue Uyetake <sue.uyetake@gmail.com>
2015-10-13 21:43:18 -0600

    made a few more changes..

--- play_array/play_array.rb
+++ play_array/play_array.rb
@@ -2,8 +2,11 @@
 
 # experimenting with arrays
 
-madlib_field = []
-madlib_field = ["these", "will", "get", "overwritten"]
+#  array = [1,2,3,4,5]
+#  array = %w(1 2 3 4 5)
+
+# madlib_field = ['these', 'will', 'get', 'overwritten']
+madlib_field = %w('these', 'will', 'get', 'overwritten')
 puts madlib_field
 puts madlib_field[0..2]
 puts ''
@@ -13,6 +16,5 @@ puts ''
 
 # code models for embedding arrays in string outputs puts ''
 puts 'here is the whole array: ' + madlib_field.to_s
-puts "here is the array within a string: #{madlib_field.to_s}"
-puts "here is an array element within a string: #{madlib_field[0].to_s}"
-
+puts "here is the array within a string: #{madlib_field}"
+puts "here is an array element within a string: #{madlib_field[0]}"
